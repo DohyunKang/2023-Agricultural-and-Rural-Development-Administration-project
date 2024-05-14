46a2f33d3763b67acf0c76b6e49201c0de783a34
Kangdo <166014987+KangKangDorumamu@users.noreply.github.com>
2024-05-14 18:54:59 +0900

    Create inser_data_weight.php
    insert loadcell sensor data to database code

---
+++ inser_data_weight.php
@@ -0,0 +1,47 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+// 데이터베이스에 입력받은 데이터를 삽입하는 코드
+
+// 수신된 데이터 사용 예시
+$deviceName = $data['device_name'];
+
+// 센서 데이터 배열에서 데이터 추출
+$sensors = $data['sensors'];
+
+foreach ($sensors as $sensor) {
+    $weight = round($sensor['weight'], 1);
+
+    // 데이터 출력
+    echo "Device Name: " . $deviceName . "<br>";
+    echo "Weight: " . $weight . "<br>";
+
+    // 데이터베이스에 저장 예시 (MySQL 사용)
+    $servername = "localhost";
+    $username = " ";
+    $password = " ";
+    $dbname = " ";
+
+    // 데이터베이스 연결
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // 데이터 삽입 쿼리
+    $sql = "INSERT INTO Loadcell_test (device_name, weight)
+            VALUES ('$deviceName', $weight)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Data inserted successfully into the database.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // 데이터베이스 연결 종료
+    $conn->close();
+}
+?>
