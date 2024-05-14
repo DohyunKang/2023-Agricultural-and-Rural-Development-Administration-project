9782bb36bb917055e5776bf0f3af05d0aef07adf
Kangdo <166014987+KangKangDorumamu@users.noreply.github.com>
2024-05-14 19:14:15 +0900

    Create insert_photo_2.php

---
+++ insert_photo_2.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set('Asia/Seoul');
+
+$str = file_get_contents('php://input');
+
+$upload_path = __DIR__ . '/beeCounter_image_2/'; // 폴더 경로 지정
+
+$latest_path = $upload_path . 'bee.jpg';
+
+file_put_contents($latest_path, $str);
+
+?>
