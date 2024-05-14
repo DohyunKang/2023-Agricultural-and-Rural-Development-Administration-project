53afa4857f0e3c8463e7390796b277b110111e43
Kangdo <166014987+KangKangDorumamu@users.noreply.github.com>
2024-05-14 19:15:12 +0900

    Create post-esp-Counter.php

---
+++ post-esp-Counter.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+$servername = "localhost";
+
+// REPLACE with your Database name
+$dbname = " ";
+// REPLACE with Database user
+$username = " ";
+// REPLACE with Database user password
+$password = " ";
+
+
+
+$api_key= $model = $innumber = $outnumber = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $api_key = test_input($_POST["api_key"]);
+    if($api_key == $api_key_value) {
+        $model = test_input($_POST["model"]);
+        $innumber = test_input($_POST["innumber"]);
+        $outnumber = test_input($_POST["outnumber"]);
+        
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        
+        $sql = "INSERT INTO beeCounter (model, innumber, outnumber)
+        VALUES ('" . $model . "', '" . $innumber . "', '" . $outnumber . "')";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } 
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    
+        $conn->close();
+    }
+    else {
+        echo "Wrong API Key provided.";
+    }
+
+}
+else {
+    echo "No data posted with HTTP POST.";
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
